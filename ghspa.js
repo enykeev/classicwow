4723c922ae791ba35b674a43a8255842c9d94c61
Kirill Enykeev <enykeev@mech.sh>
2019-08-26 20:31:31 +0700

    fix typo

--- ghspa.js
+++ ghspa.js
@@ -27,7 +27,7 @@
    /* redirect all 404 trafic to index.html */
    function redirect() {
      l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + '/?' +
-              (repo ? 'r=' + repo) + 
+              (repo ? 'r=' + repo : '') + 
               (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
               (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
               (l.hash))
